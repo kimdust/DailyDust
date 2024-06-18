6ad0244cbd4958ebbcdbb363ab8f7e71112ed687
kimdust <mji1223@naver.com>
2024-06-18 13:19:38 +0900

    readme

--- README.md
+++ README.md
@@ -1,37 +1,3 @@
-🎵 DUST Music 웹사이트 만들기  
-=============
+# 먼지의 하루
 
-### 2023.02 ~ 2024.03     
-:point_right: [깃허브 페이지] (https://kimdust.me/DustMusic)  
-
-<br/>
-<br/>
-
-
-<img width="100%" src="https://github.com/kimdust/LaMer/assets/151487785/7d2642ba-b383-4c4a-b81c-46fb04f3f92e">
-
-#### Main (https://kimdust.me/DustMusic)
-
-<br/>
-
-<img width="100%" src="https://github.com/kimdust/LaMer/assets/151487785/655dabe0-0f82-4d7f-8381-08656b50d6df">
-
-#### Music Player (https://kimdust.me/DustMusic/music/0)
-
-<br/>
-
-<img width="100%" src="https://github.com/kimdust/LaMer/assets/151487785/511795e0-d804-44e4-8859-5b3df929f0d6">
-
-#### 둘러보기 (https://kimdust.me/DustMusic/around)
-
-<br/>
-
-<img width="100%" src="https://github.com/kimdust/LaMer/assets/151487785/bc4dec95-836e-43c1-b162-9fd946a8b00a">
-
-#### 앨범 소개 (https://kimdust.me/DustMusic/album/1)
-
-<br/>
-
-<img width="100%" src="https://github.com/kimdust/LaMer/assets/151487785/2ed1b7c1-e8e9-44f2-9258-6d40e2e967b9">
-
-#### Top 50 (https://kimdust.me/DustMusic/chart)
+:point_right: [깃허브 페이지] (https://kimdust.me/DailyDust)
--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./assets/css/app.css";
 import { Flex } from "@chakra-ui/react";
+import localforage from "localforage";
+
+localforage.config({
+  driver: localforage.INDEXEDDB,
+  name: "dailyDust",
+  storeName: "diaryImages",
+});
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(
@@ -13,32 +20,52 @@ function App() {
     return savedEntries ? JSON.parse(savedEntries) : {};
   });
   const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const formattedDate = selectedDate.toISOString().split("T")[0];
-    const savedEntries = localStorage.getItem("diaryEntries");
-    const parsedEntries = savedEntries ? JSON.parse(savedEntries) : {};
-
-    if (parsedEntries[formattedDate]) {
-      setText(parsedEntries[formattedDate]);
+    if (entries[formattedDate]) {
+      setText(entries[formattedDate].text || "");
+      if (entries[formattedDate].imageKey) {
+        localforage.getItem(entries[formattedDate].imageKey).then((data) => {
+          setImage(data);
+        });
+      } else {
+        setImage(null);
+      }
     } else {
       setText("");
+      setImage(null);
     }
-  }, [selectedDate]);
+  }, [selectedDate, entries]);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
-  const handleSaveEntry = () => {
+  const handleSaveEntry = async () => {
     const formattedDate = selectedDate.toISOString().split("T")[0];
-    const updatedEntries = {
-      ...entries,
-      [formattedDate]: text,
-    };
-    setEntries(updatedEntries);
-    localStorage.setItem("diaryEntries", JSON.stringify(updatedEntries));
+    if (text.trim() === "") {
+      const { [formattedDate]: value, ...remainingEntries } = entries;
+      setEntries(remainingEntries);
+      localStorage.setItem("diaryEntries", JSON.stringify(remainingEntries));
+      if (value && value.imageKey) {
+        await localforage.removeItem(value.imageKey);
+      }
+    } else {
+      const imageKey = formattedDate + "-image";
+      const updatedEntries = {
+        ...entries,
+        [formattedDate]: { text, imageKey: image ? imageKey : null },
+      };
+      setEntries(updatedEntries);
+      localStorage.setItem("diaryEntries", JSON.stringify(updatedEntries));
+      if (image) {
+        await localforage.setItem(imageKey, image);
+      }
+    }
     setIsEditing(false);
   };
 
@@ -50,11 +77,46 @@ function App() {
     setText(e.target.value);
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const tileContent = ({ date, view }) => {
+    const formattedDate = date.toISOString().split("T")[0];
+    if (
+      view === "month" &&
+      entries[formattedDate] &&
+      entries[formattedDate].text.trim() !== ""
+    ) {
+      return <div className="entry-dot"></div>;
+    }
+    return null;
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="App">
       <h1>먼지의 하루</h1>
       <Flex>
-        <Calendar onChange={handleDateChange} value={selectedDate} />
+        <Calendar
+          onChange={handleDateChange}
+          value={selectedDate}
+          tileContent={tileContent}
+        />
         <Flex flexDirection="column">
           <h2>오늘의 일기</h2>
           <textarea
@@ -62,14 +124,54 @@ function App() {
             readOnly={!isEditing}
             onChange={handleTextChange}
             placeholder="일기를 작성하세요..."
+            spellCheck={false}
           />
-          {isEditing ? (
-            <button onClick={handleSaveEntry}>저장</button>
-          ) : (
-            <button onClick={handleEditEntry}>수정</button>
+          {image && (
+            <img
+              src={image}
+              alt="일기 이미지"
+              onClick={openModal}
+              style={{ maxWidth: "100%", marginTop: "10px" }}
+            />
           )}
+          <Flex>
+            {isEditing && (
+              <>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  id="file-input"
+                  style={{ display: "none" }}
+                />
+                <label htmlFor="file-input" className="custom-file-upload">
+                  이미지 업로드
+                </label>
+              </>
+            )}
+
+            {isEditing ? (
+              <button class={"save_btn"} onClick={handleSaveEntry}>
+                저장
+              </button>
+            ) : (
+              <button class={"edit_btn"} onClick={handleEditEntry}>
+                수정
+              </button>
+            )}
+          </Flex>
         </Flex>
       </Flex>
+      {isModalOpen && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close-button" onClick={closeModal}>
+              &times;
+            </span>
+            <img src={image} alt="일기 이미지" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
